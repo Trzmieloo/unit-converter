99c44e64f6aed771ad6b9e03818149ce0ac4a418
Trzmieloo <50712825+Trzmieloo@users.noreply.github.com>
2019-06-07 13:37:11 +0200

    problem fix

--- main2.js
+++ main2.js
@@ -18,68 +18,68 @@ function myResult(){
     resultTypeValue = resultType.value;
 
     if(inputTypeValue === "meters" && resultTypeValue === "kilometers"){
-        result.value = Number(imput.value) * 0.001;
+        result.value = Number(input.value) * 0.001;
     
     }else if(inputTypeValue === "meters" && resultTypeValue === "miles"){
-        result.value = Number(imput.value) * 0.0006;
+        result.value = Number(input.value) * 0.0006;
     
     }else if(inputTypeValue === "meters" && resultTypeValue === "meters"){
-        result.value = imput.value;
+        result.value = input.value;
 
     }
 
     if(inputTypeValue === "kilometers" && resultTypeValue === "meters"){
-        result.value = Number(imput.value) * 1000;
+        result.value = Number(input.value) * 1000;
     
     }else if(inputTypeValue === "kilometers" && resultTypeValue === "miles"){
-        result.value = Number(imput.value) * 0.6;
+        result.value = Number(input.value) * 0.6;
     
     }else if(inputTypeValue === "kilometers" && resultTypeValue === "kilometers"){
-        result.value = imput.value;
+        result.value = input.value;
 
     }
 
     if(inputTypeValue === "miles" && resultTypeValue === "meters"){
-        result.value = Number(imput.value) * 1609;
+        result.value = Number(input.value) * 1609;
     
     }else if(inputTypeValue === "miles" && resultTypeValue === "kilometers"){
-        result.value = Number(imput.value) * 1.6;
+        result.value = Number(input.value) * 1.6;
     
     }else if(inputTypeValue === "miles" && resultTypeValue === "miles"){
-        result.value = imput.value;
+        result.value = input.value;
 
     }
 
     if(inputTypeValue === "kilograms" && resultTypeValue === "grams"){
-        result.value = Number(imput.value) * 1000;
+        result.value = Number(input.value) * 1000;
     
     }else if(inputTypeValue === "kilograms" && resultTypeValue === "pounds"){
-        result.value = Number(imput.value) * 2.2;
+        result.value = Number(input.value) * 2.2;
     
     }else if(inputTypeValue === "kilograms" && resultTypeValue === "kilograms"){
-        result.value = imput.value;
+        result.value = input.value;
 
     }
 
     if(inputTypeValue === "grams" && resultTypeValue === "kilograms"){
-        result.value = Number(imput.value) * 0.001;
+        result.value = Number(input.value) * 0.001;
     
     }else if(inputTypeValue === "grams" && resultTypeValue === "pounds"){
-        result.value = Number(imput.value) * 0.002;
+        result.value = Number(input.value) * 0.002;
     
     }else if(inputTypeValue === "grams" && resultTypeValue === "grams"){
-        result.value = imput.value;
+        result.value = input.value;
 
     }
 
     if(inputTypeValue === "pounds" && resultTypeValue === "kilograms"){
-        result.value = Number(imput.value) * 0.45;
+        result.value = Number(input.value) * 0.45;
     
     }else if(inputTypeValue === "pounds" && resultTypeValue === "grams"){
-        result.value = Number(imput.value) * 453;
+        result.value = Number(input.value) * 453;
     
     }else if(inputTypeValue === "pounds" && resultTypeValue === "pounds"){
-        result.value = imput.value;
+        result.value = input.value;
 
     }
 
